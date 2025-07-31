8b4f7023c7b5e01f62fbf41a5dbe3d21f48c7b51
Snowin Cafasso <125958090+SnowinCafasso@users.noreply.github.com>
2025-07-31 14:51:26 +0530

    Delete maverick-bank-current-copy directory (old copy)

--- maverick-bank-current-copy/src/main/java/com/example/maverickBank/repository/AdminRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.maverickBank.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.maverickBank.entity.Admin;
-
-public interface AdminRepository extends JpaRepository<Admin, Long>{
-
-}
--- maverick-bank-current-copy/src/main/java/com/example/maverickBank/service/AdminService.java
+++
@@ -1,7 +0,0 @@
-package com.example.maverickBank.service;
-
-import com.example.maverickBank.entity.Admin;
-
-public interface AdminService {
-	Admin createAdmin(Admin admin);
-}
